323582e7ba6f75db572d595e493b8cc0ee1c32bf
antonin clt <collot@et.esiea.fr>
2022-10-10 18:26:55 +0200

    FilteredLogger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -6,6 +6,7 @@ public class LoggerFactory {
     public static Logger getLogger(String name) {
         Logger logger = new ContextualLogger(name, new FileLogger("log.txt"));
         Logger compositeLogger = new CompositeLogger(logger, new ConsoleLogger());
+        Logger filteredLogger = new FilteredLogger(new FileLogger("log.txt"), message -> message.contains("Simulation"));
         return compositeLogger;
     }
 }
